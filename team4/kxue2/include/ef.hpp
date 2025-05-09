c9a71c316dde9175c7c485382ebaa9dcf11144b8
RamenMode <xuek2022@outlook.com>
2025-05-09 05:18:45 +0000

    Kevin Xue Project

---
+++ team4/kxue2/include/ef.hpp
@@ -0,0 +1,13 @@
+#include <etherfabric/vi.h>
+#include <etherfabric/pd.h>
+#include <etherfabric/memreg.h>
+#include <etherfabric/capabilities.h>
+#include <etherfabric/efct_vi.h>
+#include <etherfabric/base.h>
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
